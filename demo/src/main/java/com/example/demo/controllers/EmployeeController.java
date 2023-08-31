770acea8de83e8bb2dd2154090dc054f7f8b3c61
gusmgoes <ggoes269@gmail.com>
2023-08-31 13:12:58 -0400

    Adding CORS configuration

--- demo/src/main/java/com/example/demo/controllers/EmployeeController.java
+++ demo/src/main/java/com/example/demo/controllers/EmployeeController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/employees")
 @RestController
+@RequestMapping("/employees")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class EmployeeController {
 
     private final EmployeeService employeeService;
--- demo/src/main/java/com/example/demo/controllers/GameController.java
+++ demo/src/main/java/com/example/demo/controllers/GameController.java
@@ -14,14 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RequestMapping("/games")
 @RestController
+@RequestMapping("/games")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class GameController {
 
     private final GameService gameService;
